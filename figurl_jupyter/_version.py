3d6f7821d5e187e184a6ec481edbadda202c4f79
Jeremy Magland <jmagland@flatironinstitute.org>
2022-09-02 11:33:29 -0400

    update version

--- figurl_jupyter/_version.py
+++ figurl_jupyter/_version.py
@@ -4,6 +4,6 @@
 # Copyright (c) Jeremy Magland.
 # Distributed under the terms of the Modified BSD License.
 
-# version_info = (0, 2, 4, 'dev')
-version_info = (0, 2, 4)
+version_info = (0, 2, 5, 'dev')
+# version_info = (0, 2, 5)
 __version__ = ".".join(map(str, version_info))
