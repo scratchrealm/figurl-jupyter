43f0e20486bc5c7cbaff5ad2715bee2f538a4e6b
Jeremy Magland <jmagland@flatironinstitute.org>
2022-09-28 15:43:43 -0400

    prepare release 0.2.8

--- figurl_jupyter/_version.py
+++ figurl_jupyter/_version.py
@@ -4,6 +4,6 @@
 # Copyright (c) Jeremy Magland.
 # Distributed under the terms of the Modified BSD License.
 
-# version_info = (0, 2, 6, 'dev')
-version_info = (0, 2, 6)
+# version_info = (0, 2, 8, 'dev')
+version_info = (0, 2, 8)
 __version__ = ".".join(map(str, version_info))
